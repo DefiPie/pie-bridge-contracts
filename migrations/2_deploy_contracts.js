151600ce4ee5c2b6acc5afe414006703e5c3f4af
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-02-18 16:09:19 +0300

    replace address in deployer for bsc

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,23 @@
 // for deploy:
 // > truffle migrate --network rinkeby
+// or
+// > truffle migrate --network bsctestnet
 // for verify:
-// truffle run verify PieBridge@0xAcCd67fe87b8A657e9dCCFAE2770e49260f9c034 --network rinkeby
+// > truffle run verify PieBridge@0xb33722ae32B6C854a04fb0cF0D709aFC5b97BAfB --network rinkeby
+// or
+// > truffle run verify PieBridge@0xd572aebED5233418747bE456E49e5b72F283826A --network bsctestnet
 
 const Bridge = artifacts.require("PieBridge");
 
 module.exports = async function(deployer) {
-    let admin = '0x9D19Be3814dB048C73F804db05d59a3EBd0F20B3';
+    let courier = '0x9D19Be3814dB048C73F804db05d59a3EBd0F20B3';
 
     let pieBSC = '0xd50da88069c69BF093e8dca1532Cc81711D9e0F4';
     let pieRinkeby = '0xb36afc9f38d8ac6f991bb9939d3ee8d45a7a1285';
 
     await deployer.deploy(Bridge,
-        admin,
-        pieRinkeby
+        courier,
+        pieBSC
     );
 
     const bridge = await Bridge.deployed();
