7f1711423251a8cba06a7d2bff80715eee279b6c
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-02-21 23:23:08 +0300

    fix deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,9 @@
 // or
 // > truffle migrate --network bsctestnet
 // for verify:
-// > truffle run verify PieBridge@0xb33722ae32B6C854a04fb0cF0D709aFC5b97BAfB --network rinkeby
+// > truffle run verify PieBridge@0x3b80ebf98f45f5fccec7a915632a0a2e58bff2e6# --network rinkeby
 // or
-// > truffle run verify PieBridge@0xd572aebED5233418747bE456E49e5b72F283826A --network bsctestnet
+// > truffle run verify PieBridge@0x600e743cbb8eE5856A6e028Ac0bef487061F435d --network bsctestnet
 
 const Bridge = artifacts.require("PieBridge");
 
@@ -14,10 +14,12 @@ module.exports = async function(deployer) {
 
     let pieBSC = '0xd50da88069c69BF093e8dca1532Cc81711D9e0F4';
     let pieRinkeby = '0xb36afc9f38d8ac6f991bb9939d3ee8d45a7a1285';
+    let fee = '0';
 
     await deployer.deploy(Bridge,
         courier,
-        pieBSC
+        pieRinkeby,
+        fee
     );
 
     const bridge = await Bridge.deployed();
