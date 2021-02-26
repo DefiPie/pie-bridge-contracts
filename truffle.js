bf8ff440d6ea38e105f1c64bf4d4b3466f4c710f
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-02-26 13:40:03 +0300

    add networks to config file

--- truffle.js
+++ truffle.js
@@ -1,19 +1,14 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
 
-var mnemonic = process.env.NMONIC;
-var rinkebyClientUrl = process.env.RINKEBY_CLIENT_URL;
 
 module.exports = {
     compilers: {
         solc: {
             version: "0.7.6",    // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
             settings: {          // See the solidity docs for advice about optimization and evmVersion
              optimizer: {
                enabled: false,
                runs: 200
              },
-            //  evmVersion: "byzantium"
             }
         }
     },
@@ -32,23 +27,16 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    // rinkeby: {
-    //   provider: function() {
-    //     return new HDWalletProvider(mnemonic, rinkebyClientUrl);},
-    //   network_id: 4,
-    //   gas: 4612388 // Gas limit used for deploys
-    // },
+    mainnet: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "1" // Match any network id
+    },
     rinkeby: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "4" // Match any network id
     },
-    // bsctestnet: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
-    //   network_id: 97,
-    //   timeoutBlocks: 200,
-    //   confirmations: 5
-    // },
     bsc: {
       host: "127.0.0.1",
       port: 8575,
@@ -61,7 +49,7 @@ module.exports = {
       host: "127.0.0.1",
       port: 8575,
       network_id: "97", // Match any network id,
-      confirmations: 5,
+      confirmations: 3,
       timeoutBlocks: 200,
       skipDryRun: true
     }
