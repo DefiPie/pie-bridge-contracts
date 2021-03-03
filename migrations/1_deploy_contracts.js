46664820803b75b8144ef79ffc454c1041b46bd2
maxxttor <maxxttor@gmail.com>
2021-03-03 13:29:30 +0300

    update deploye script

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -15,7 +15,7 @@ const Proxy = artifacts.require("BridgeProxy");
 module.exports = async function(deployer, network, accounts) {
 
     let courier = '0x9D19Be3814dB048C73F804db05d59a3EBd0F20B3';
-    let guardian;
+    let guardian = '0x6EBD066e2891922f3c1BfD426B2a49D5D0bc11C8';
     let pie;
     let fee = '1';
     let routes;
@@ -23,19 +23,15 @@ module.exports = async function(deployer, network, accounts) {
     if (network === 'bsctestnet') {
         pie = '0xd50da88069c69BF093e8dca1532Cc81711D9e0F4';
         routes = ['3', '4'];
-        guardian = deployer.networks.bsctestnet.from;
     } else if (network === 'rinkeby') {
         pie = '0xb36afc9f38d8ac6f991bb9939d3ee8d45a7a1285';
         routes = ['3','97'];
-        guardian = deployer.networks.rinkeby.from;
     } else if (network === 'mainnet') {
         pie = '0x607C794cDa77efB21F8848B7910ecf27451Ae842';
         routes = ['56'];
-        guardian = deployer.networks.mainnet.from;
     } else if (network === 'bsc') {
         pie = '0xC4B35d3A24E3e8941c5d87fD21D0725642F50308';
         routes = ['1'];
-        guardian = deployer.networks.bsc.from;
     } else {
         console.log('Pie is not defined');
         console.log('Routes are not defined');
